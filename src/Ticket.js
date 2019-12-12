53f21bf53b165dd8e3e4eba0d5cfe841ca607caf
lszybka <lukasz.szybka@mobica.com>
2019-12-12 08:44:41 +0100

    Added flip animation

--- src/Ticket.js
+++ src/Ticket.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 class Ticket extends Component {
     constructor() {
@@ -10,13 +11,23 @@ class Ticket extends Component {
     }
 
     showBackImage = () => {
-        this.setState( { showBack: !this.state.showBack} )
+        if (this.props.data.imageBack !== undefined && this.props.data.imageBack !== '') {
+            this.setState( { showBack: !this.state.showBack} )
+        }
     }
 
     render() {
         return (
-            <div className='bg-light-blue dib grow br3 pa2 ma3 bw2 shadow-5 tc' onClick = { this.showBackImage }>
-                <img alt = '' src={ this.state.showBack ? this.props.data.imageBack : this.props.data.imageFront }></img>
+            <div className='dib br3 ma3 bw2 shadow-5 tc' onClick = { this.showBackImage }>
+                <ReactCardFlip isFlipped={this.state.showBack} flipSpeedBackToFront = {0.5}>
+                    <div>
+                        <img alt = '' src={ this.props.data.imageFront }></img>
+                    </div>
+
+                    <div>
+                        <img alt = '' src={ this.props.data.imageBack }></img>
+                    </div>
+                </ReactCardFlip>
             </div>
           );
     }
